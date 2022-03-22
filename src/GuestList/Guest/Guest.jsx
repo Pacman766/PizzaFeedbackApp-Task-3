62e9c9836970e10fca8fece860c9f75093c841b5
unknown <3334976@gmail.com>
2022-03-22 06:46:06 +0300

    add comment

--- src/GuestList/Guest/Guest.jsx
+++ src/GuestList/Guest/Guest.jsx
@@ -5,10 +5,12 @@ import { getFeedbacksFromLocalStorage } from "../../localStorage/localStorageUti
 
 
 const Guest = ({ name, eatsPizza, diets, onClick }) => {
+  // arr of vegans names
   let vegans = diets.filter((person) => person.isVegan).map((it) => it.name);
 
   const [feedback, setFeedback] = useState(undefined);
 
+  // if we have feedback on LS, find and set feedback name 
   useEffect(() => {
     const feedBacksFromLocalStorage = getFeedbacksFromLocalStorage();
     if (feedBacksFromLocalStorage && feedBacksFromLocalStorage.length) {
